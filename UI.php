b1e5d1936edb2f41f046d65ddbddd5afd9efa293
Stig Rune <stig.rune@haugnett.no>
2012-09-15 16:10:40 +0200

    Added some comments to UI.php

--- UI.php
+++ UI.php
@@ -1,16 +1,27 @@
 <?php
 /*
 * PHP User Interface Class
+*
+* @author 		Stig Rune
+* @version		1.0
+* 
 */
 ?>
 
 <?php
+	/*
+		This class is used only as a HTMLElement factory,
+		the name is short to make our code less bloat.
+	 */
 	class UI {
 		public static function __callStatic($function, $vars) {
 			return new HTMLElement($function);
 		}
 	}
 
+	/*
+		HTMLElement represents a single html element.
+	 */
 	class HTMLElement {
 		public function __construct($tag) {
 			$this->tag = $tag;
@@ -21,13 +32,16 @@
 			return $this;
 		}
 
+		// Overloaded __call to make dynamic handling of attributes.
 		public function __call($name, $arguments) {
 			$name = strtolower($name);
 			$reserved = array("tag", "inner", "children");
 
 			if (in_array($name, $reserved)) {
+				// Handle local properties.
 				$this->$name = $arguments[0];
 			} else {
+				// Handle attributes on the element.
 				if (trim($arguments[0])) {
 					$this->attributes[$name] = $arguments[0];
 				}
@@ -36,6 +50,8 @@
 			return $this;
 		}
 
+		// Overloaded __toString to make the representation 
+		// of the class HTML text.
 		public function __toString() {
 			$html = "<{$this->tag}";
 			
