cf5e2cf12be772d5c4b283aa426dc075a24c1495
Stig Rune <stig.rune@haugnett.no>
2013-02-11 16:01:46 +0100

    Version 1.1
    Added proper documentation.
    Made it possible to set attributes like data-validation using the
    formating data_validation() - exchanging the dash for a underscore.

--- UI.php
+++ UI.php
@@ -1,28 +1,38 @@
 <?php
-/*
-* PHP User Interface Class
+/**
+* UI/HTMLElement - makes HTML outputting using PHP a breeze.
 *
-* @author 		Stig Rune
-* @version		1.0
-* 
+* @author 		Stig Rune Grønnestad
+* @copyright	Stig Rune Grønnestad @ 2013
+* @version		1.1
+*
 */
-?>
 
-<?php
-	/*
-		This class is used only as a HTMLElement factory,
-		the name is short to make our code less bloat.
+	/**
+	* This class is used only as a HTMLElement factory,
+	* the name is short to make our code less bloat.
 	 */
 	class UI {
+		/**
+		 * HTMLElement fabrication. This class makes it possible to initialize
+		 * objects using UI::div(), UI::table() and so forth.
+		 * @param  [string] $function "Function" or name of the element to fabricate
+		 * @param  [type] $vars     not used
+		 * @return [HTMLElement]
+		 */
 		public static function __callStatic($function, $vars) {
 			return new HTMLElement($function);
 		}
 	}
 
-	/*
-		HTMLElement represents a single html element.
+	/**
+	 * HTMLElement represents a single html element.
 	 */
 	class HTMLElement {
+		/**
+		 * Constructor for the base HTMLElement
+		 * @param [string] $tag tag name
+		 */
 		public function __construct($tag) {
 			$this->tag = $tag;
 			$this->inner = "";
@@ -32,7 +42,12 @@
 			return $this;
 		}
 
-		// Overloaded __call to make dynamic handling of attributes.
+		/**
+		 * Override call function to have it set attributes on the element instead
+		 * @param  [string] $name      attribute name
+		 * @param  [mixed] $arguments attribute value
+		 * @return [HTMLElement]	 self
+		 */
 		public function __call($name, $arguments) {
 			$name = strtolower($name);
 			$reserved = array("tag", "inner", "children");
@@ -43,6 +58,7 @@
 			} else {
 				// Handle attributes on the element.
 				if (trim($arguments[0])) {
+					$name = str_replace("_", "-", $name);
 					$this->attributes[$name] = $arguments[0];
 				}
 			}
@@ -50,11 +66,13 @@
 			return $this;
 		}
 
-		// Overloaded __toString to make the representation 
-		// of the class HTML text.
+		/**
+		 * Print out the HTML for the element when used as string
+		 * @return string HTML of element
+		 */
 		public function __toString() {
 			$html = "<{$this->tag}";
-			
+
 			// Add all the attributes
 			foreach ($this->attributes as $key => $value) {
 				$html .= " {$key}=\"{$value}\"";
